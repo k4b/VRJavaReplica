49af5c919b0490dfa4209c700118ca3b62c6faa0
karol <karol@linux>
2013-07-03 15:17:27 +0200

    Simple multithreaded server on sockets

---
+++ src/vrjavareplica/VRCode.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vrjavareplica;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author karol
+ */
+public class VRCode implements Runnable{
+    
+    private Replica replica;
+    protected ServerSocket serverSocket = null;
+    protected boolean      isStopped    = false;
+    protected Thread       runningThread= null;
+    
+    public VRCode(Replica replica) {
+        this.replica = replica;
+    }
+
+    public void run(){
+        synchronized(this){
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+        while(! isStopped()){
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if(isStopped()) {
+                    System.out.println("Server Stopped.") ;
+                    return;
+                }
+                throw new RuntimeException("Error accepting client connection", e);
+            }
+            new Thread(
+                new WorkerRunnable(clientSocket, "Multithreaded Replica Server")
+            ).start();
+        }
+        System.out.println("Server Stopped.") ;
+    }
+
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop(){
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.replica.getPort());
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open port " + this.replica.getPort(), e);
+        }
+    }
+    
+}
